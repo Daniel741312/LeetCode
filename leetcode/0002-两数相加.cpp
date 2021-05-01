375a6977c00093f8b4e0b0c8b5424efbd0cdba3f
Daniel741312 <syc198@qq.com>
2021-05-01 22:04:07 +0800

    两数相加

--- leetcode/0002-两数相加.cpp
+++ leetcode/0002-两数相加.cpp
@@ -9,32 +9,51 @@ struct ListNode {
     ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
+/*调试函数*/
 ListNode* makeAList(std::vector<int>& array);
 void printList(ListNode* l);
 
 class Solution {
-   public:
-    bool isPalindrome(ListNode* head) {
-        std::vector<int> temp;
-        while(head!=nullptr){
-            temp.emplace_back(head->val);
-            head=head->next;
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head=nullptr;
+        ListNode* tail=nullptr;
+        int carray=0;
+        while(l1||l2){
+            int n1=l1?l1->val:0;
+            int n2=l2?l2->val:0;
+            int sum=n1+n2+carray;
+            carray=sum/10;
+            if(head==nullptr){
+                head=tail=new ListNode(sum%10);
+            }else{
+                tail->next=new ListNode(sum%10);
+                tail=tail->next;
+            }
+            if(l1!=nullptr){
+                l1=l1->next;
+            }
+            if(l2!=nullptr){
+                l2=l2->next;
+            }
         }
-        for(unsigned int i=0,j=temp.size()-1;i<j;i++,j--){
-            if(temp[i]!=temp[j])
-                return false;
+        if(carray!=0){
+            tail->next=new ListNode(carray);
         }
-        return true;
+        return head;
     }
 };
 
 int main(int argc,char* argv[]){
     Solution* s0=new Solution();
+    std::vector<int> v1={9,9,9,9,9,9,9};
+    std::vector<int> v2={9,9,9,9};
+    
+    ListNode* l1=makeAList(v1);
+    ListNode* l2=makeAList(v2);
 
-    std::vector<int> arr={1,2,2,1};
-    ListNode* head=makeAList(arr);
-    printList(head);
-    std::cout<<s0->isPalindrome(head)<<std::endl;
+    ListNode* ret=s0->addTwoNumbers(l1,l2);
+    printList(ret);
 
     delete(s0);
     s0=nullptr;
